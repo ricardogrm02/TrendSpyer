c1b1c507a43cf981185c865682f801c019626656
ricardogrm02 <ricardog2002@csu.fullerton.edu>
2024-03-30 00:02:49 -0700

    TEAM-83 Delete Location through longitude and latitude

--- TrendSpyerApp/routes/mapRoute.js
+++ TrendSpyerApp/routes/mapRoute.js
@@ -14,5 +14,34 @@ router.get("/info", async (req, res) => {
       res.status(500).json({ message: "Internal server error" });
     }
   });
+  router.post("/location", async (req, res) => {
+    try {
+      const location = await Map.create({
+       LocationName: req.body.LocationName,
+       latitude: req.body.latitude,
+       longitude: req.body.longitude,
+      })
+      res.status(200).send("Location of intrest added to database")
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  });
+
+  router.delete('/remove/location', async (req, res) => {
+    try {
+        const location = await Map.findOneAndDelete({latitude: req.body.latitude, longitude: req.body.longitude});
+        if (!location) {
+            return res.status(404).send(); // Send 404 if no blog was found
+        }
+        res.send(`Successfuly deleted location: ${location}`); // Send deleted blog
+    } catch (error) {
+        res.status(500).send(error); // Send 500 if an error occurs
+    }
+});
+
+  
+
+  
 
   module.exports = router;
