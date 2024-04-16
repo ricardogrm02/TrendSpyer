3d311a363685e226f64fa19c204cb29ac367b704
ricardogrm02 <ricardog2002@csu.fullerton.edu>
2024-04-16 13:49:16 -0700

    Set Initial Route to be the Map Screen

--- TrendSpyerApp/TestDisplay.js
+++ TrendSpyerApp/TestDisplay.js
@@ -22,7 +22,7 @@ const App = () => {
   return (
     <PaperProvider theme={theme}>
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator initialRouteName="MapScreen">
         <Stack.Screen
             name="MapScreen"
             component={MapScreen}
