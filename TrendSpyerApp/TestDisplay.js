352ecb8eac2b672c14f869a8d8ef0eee43c7edc8
ricardogrm02 <ricardog2002@csu.fullerton.edu>
2024-04-23 09:47:34 -0700

    Set Initial Screen to Login Screen for Demo

--- TrendSpyerApp/TestDisplay.js
+++ TrendSpyerApp/TestDisplay.js
@@ -24,7 +24,7 @@ const App = () => {
   return (
     <PaperProvider theme={theme}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Register">
+        <Stack.Navigator initialRouteName="LoginScreen">
 
         <Stack.Screen
             name="RegisterScreen"
